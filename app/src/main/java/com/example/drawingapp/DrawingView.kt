8a725b073af9249279932e41b747040dd90d1b35
TaeHoonIm <84333006+TaeHoonIm@users.noreply.github.com>
2023-03-07 00:31:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,113 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap : Bitmap? = null
+    private var mDrawPaint : Paint? = null
+    //canvasPaint 뷰의 instance(->객체가 메모리에 할당되어 실제 사용하는 경우)
+    private var mCanvasPaint : Paint? = null
+    private var mBrushSize : Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas : Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+    }
+    //화면 크기가 바뀔 때마다 불러옴. 즉, DrawingView가 표시될 때
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        //ARGB_8888 -> 사용 가능한 색
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // Canvas가 에러나면 Canvas?로 수정
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+
+        }
+
+        if(!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+    //화면을 터치했을 때 그림그리기 시작
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        //화면을 눌렀을 때 실행할 행동
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                // !!보다 강력하게 조건문을 걸어 null이 아닐 때 실행
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX,touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE ->{
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+        //뷰가 화면에 보일 때 전체 뷰의 모든 이미지를 없앰
+        invalidate()
+
+        return true
+    }
+
+    fun setSizeForBrush(newSize : Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    //코틀린에서는 내부 클래스가 기본적으로 외부 클래스를 참조하여 객체에 접근이 가능.
+    internal inner class CustomPath(var color: Int,
+                                    var brushThickness: Float) : Path() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+    }
+}
